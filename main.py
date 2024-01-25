6165bd307fd367c92e6c6089ea2fea80b1af4483
YaBraG <92254077+YaBraG@users.noreply.github.com>
2024-01-25 18:39:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
 
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
+        x=input("Enter Pan and Tilt separated with a semicolon (;)")
+        ser.write(x)
+        # line = ser.readline().decode('utf-8').rstrip()
+        # print(line)
         time.sleep(1)
