9b61e745c913a75c42316ca5a57bc4e2cf0f5f03
YaBraG <92254077+YaBraG@users.noreply.github.com>
2024-01-26 13:23:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import serial
 import time
 
-line = "Sended"
-
 if __name__ == '__main__':
     try:
         ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
@@ -13,8 +11,8 @@ if __name__ == '__main__':
 
     while True:
         x = input("Enter Pan and Tilt separated with a semicolon (;): ")
-        b = x.encode('utf-8')
+        # b = x.encode('utf-8')
         ser.write(str(x).encode('utf-8'))
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
+        # line = ser.readline().decode('utf-8').rstrip()
+        # print(line)
         time.sleep(1)
