2f182568ee68acee68a30e3e7ef29ce4b30cb4a2
YaBraG <92254077+YaBraG@users.noreply.github.com>
2024-08-12 13:13:54 -0400

    idk

--- AI.py
+++ AI.py
@@ -1,3 +1,5 @@
+'''
+                Working Ollama
 from langchain_community.llms import Ollama
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import SentenceTransformerEmbeddings
@@ -36,3 +38,46 @@ print(f"Query: {queries}\nResponse : {response}\n")
 # message = [("system","You are a helpful assistant that translates English to French. Translate the user sentence."),("human","I love the pink")]
 # ai_msg = llm.invoke(message)
 # print(ai_msg)
+'''
+
+'''
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+
+loader = UnstructuredFileLoader("ai_adoption_framework_whitepaper.pdf")
+docs = loader.load()
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=2000,
+    chunk_overlap=200
+)
+texts = text_splitter.split_documents(docs)
+
+embeddings = HuggingFaceEmbeddings()
+
+db = FAISS.from_documents(texts, embeddings)
+
+llm = Ollama(model="llama3")
+
+chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=db.as_retriever()
+)
+
+question = "Can you please summarize the document"
+result = chain.invoke({"query": question})
+
+print(result['result'])
+'''
+
+from langchain_unstructured import UnstructuredLoader
+
+loader = UnstructuredLoader("./ai_adoption_framework_whitepaper.pdf")
+docs = loader.load()
+
+print(docs[0])
